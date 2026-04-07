8032f034b684179cb55f149b9b1edeba972d7f9e
Lames-mohammed <lames.mohammed@student.tafesa.edu.au>
2026-04-07 12:31:59 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,20 +15,19 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
-
+pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients",
-    my_dataframe, max_selections=5)
+    pd_df["FRUIT_NAME"].tolist(), max_selections=5)
 st.write("You selected:", ingredients_list)
 
 if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + 'nutrition information')
+        st.subheader(fruit_chosen + ' nutrition information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
